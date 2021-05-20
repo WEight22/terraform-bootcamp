35e3a88c99b652a9560ac686f51911cbf9d60209
CloudWen_Cathay <cloudwen@cathayholdings.com.tw>
2021-05-20 12:09:13 +0800

    create day2

--- module/vpc/output.tf
+++
@@ -1,7 +0,0 @@
-output "vpc_id" {
-    value = aws_vpc.tf_vpc.id
-}
-
-output "subnet_id" {
-    value = aws_subnetc.tf_vpc_subnet1.id
-}
--- output.tf
+++
@@ -1,4 +0,0 @@
-output "Private_IP" {
-    description = "ec2 private ip"
-    value = aws_instance.web-server-exsample.private_ip
-}
