1916ecc3c493d6e6f1ed0823b3bb7e079258b84c
CloudWen_Cathay <cloudwen@cathayholdings.com.tw>
2021-05-26 01:50:38 +0800

    3rd test

--- day4/terraform-cloud/main.tf
+++ day4/terraform-cloud/main.tf
@@ -6,6 +6,6 @@ module "ec2_with_mandatory_tagging" {
   source  = "github.com/cloudtp/hello-terraform-module"
   Name = "cloud-terraform-Module2Labs-2"
   EndDate = "next-week"
-  user_id = "cloud"
+  user_id = "cloudwen"
   
 }
