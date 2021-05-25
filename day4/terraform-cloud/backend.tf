acc1bcc13702008d67557d8f839dd763dde3c217
CloudWen_Cathay <cloudwen@cathayholdings.com.tw>
2021-05-26 01:29:56 +0800

    add tf-cloud

---
+++ day4/terraform-cloud/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.42.0"
+    }
+  }
+
+  required_version = "~> 0.14"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "example-org-0eb5a7"
+
+    workspace = {
+      name = "terraform-bootcamp"
+    }
+  }
+}
