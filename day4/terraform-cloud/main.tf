e854d1ecd4ae71cd56d372c2d315f0c221fa9b0c
CloudWen_Cathay <cloudwen@cathayholdings.com.tw>
2021-05-26 13:06:49 +0800

    disable module

--- day4/terraform-cloud/main.tf
+++ day4/terraform-cloud/main.tf
@@ -1,11 +1,30 @@
+// This is an AWS provider block
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
-module "ec2_with_mandatory_tagging" {
-  source  = "github.com/cloudtp/hello-terraform-module"
-  Name = "cloud-terraform-Module2Labs-2"
-  EndDate = "next-week"
-  user_id = "cloudwen"
-  
+
+// This is an AWS resource block that creates an EC2 instance
+resource "aws_instance" "new-server-example-1" {
+  ami            = "ami-2757f631"
+  instance_type  = "t2.micro"
+  count = 5
+
+  tags = {
+    Name        = "new-server-example-1"
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
+
+# provider "aws" {
+#   region  = "us-east-1"
+# }
+
+# module "ec2_with_mandatory_tagging" {
+#   source  = "github.com/cloudtp/hello-terraform-module"
+#   Name = "cloud-terraform-Module2Labs-2"
+#   EndDate = "next-week"
+#   user_id = "cloudwen"
+  
+# }
